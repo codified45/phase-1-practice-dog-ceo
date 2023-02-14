6092db508d696d853c0a51f167fcceb33d97a908
Jack Lach <jack.lach4@gmail.com>
2023-02-14 08:39:02 -0500

    Challenge 1 complete

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+const breedUrl = "https://dog.ceo/api/breeds/list/all";
+
+const init = () => {
+    const mainDiv = document.getElementById('dog-image-container');
+    let urlArray = [];
+
+    fetch(imgUrl)
+        .then(res => res.json())
+            .then(object => {
+                urlArray = [...object["message"]];
+                urlArray.forEach(url => {
+                    const img = document.createElement('img');
+                    img.src = url;
+                    mainDiv.appendChild(img);
+                })
+                });
+                
+};
+            
+
+document.addEventListener('DOMContentLoaded', init);
+
+
+// Challenge 1: complete
+// Challenge 2: 
